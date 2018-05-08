5e63825e574d715082746518c682328647d0d135
Rafael.Gutierrez <rgutierreztejada@gmail.com>
2018-05-08 15:21:15 -0600

    WEb : Ip visistas

--- src/WebBundle/Controller/DefaultController.php
+++ src/WebBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Entity\UserViews;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -16,10 +18,33 @@ class DefaultController extends Controller
     	$em 	= $this->getDoctrine()->getManager();        
         $state 	= $em->getRepository('AppBundle:State')->findAll();
 
-        $RAW_QUERY	= "select * from user as u left join medical_detail as md on u.usr_id = md.usr_id";
+        $RAW_QUERY	= "select md.md_first_name, md.md_first_surname,c.cit_name,s.sta_name,md.md_profile_image,s.sta_code, group_concat(e.sp_name SEPARATOR ', ') as esp from user as u left join medical_detail as md on u.usr_id = md.usr_id left join contact_info as ci on ci.usr_id = u.usr_id left join city as c on c.cit_id = ci.cit_id
+			left join state as s on s.sta_id = c.sta_id
+			left join speciality as e on e.usr_id = u.usr_id group by u.usr_id";
         $statement  = $em->getConnection()->prepare($RAW_QUERY);
-        $statement->execute();    
+        			  $statement->execute();    
         $medic    	= $statement->fetchAll();
+
+        // Validar si la Cookie se ha seteado
+        if(empty($_COOKIE['contador']))
+        {
+            // Obtener La Ip del visitante.
+            $ip = $this->getRealIP();
+
+            $id_user['user'] 	= $em->getRepository('AppBundle:User')->findBy( array('usrId' => 1) );      
+
+            $visitas = new UserViews();
+            //$visitas->setVisUsu( $id_user['user'] );
+            $visitas->setVisReferencia($ip);
+            $visitas->setVisFechaCrea(new \DateTime("now"));
+
+            //$em->persist($visitas);
+            //$em->flush();
+
+            //setcookie('contador', 1, time() + 365 * 24 * 60 * 60, $request->getRequestUri());
+        }
+
+
         
         return $this->render('web/default/index.html.twig', array('state'=> $state , 'medic' => $medic ));
     }
@@ -39,5 +64,28 @@ class DefaultController extends Controller
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
+    }
+
+
+
+    private function getRealIP()
+    {
+   
+        if ($_SERVER['SERVER_NAME']) 
+        {
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR'] ))
+            {
+                $sIpAddress = @$_SERVER["HTTP_X_FORWARDED_FOR"];
+            }
+            elseif (isset($_SERVER["HTTP_CLIENT_IP"] ))
+            {
+                $sIpAddress = @$_SERVER["HTTP_CLIENT_IP"];
+            }
+            else
+            {
+                $sIpAddress = @$_SERVER["REMOTE_ADDR"];
+            }
+        }    
+        return $sIpAddress;
     }
 }
