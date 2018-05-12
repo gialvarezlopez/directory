38daaca736999e5f2a1054f36f69e8297623c1fd
dkrock24 <rgutierreztejada@gmail.com>
2018-05-12 16:19:06 -0600

    WEB : Ajustes en detalle

--- src/WebBundle/Controller/DefaultController.php
+++ src/WebBundle/Controller/DefaultController.php
@@ -43,7 +43,7 @@ class DefaultController extends Controller
         $state 	= $em->getRepository('AppBundle:State')->findAll();
         $speciality  = $em->getRepository('AppBundle:Speciality')->findAll();
 
-        $RAW_QUERY	= "select md.md_first_name, md.md_first_surname,c.cit_name,s.sta_name,md.md_profile_image,s.sta_code, 
+        $RAW_QUERY	= "select  u.usr_id, md.md_first_name, md.md_first_surname,c.cit_name,s.sta_name,md.md_profile_image,s.sta_code, 
                         ci.ci_lat,ci.ci_lng,ci.ci_address,ci.ci_phone1,
                         group_concat(e.sp_name SEPARATOR ', ') as esp, (select count(*) as totla 
                         from user_views as userV where userV.vis_usu_id = u.usr_id ) as total from user as u 
@@ -69,7 +69,7 @@ class DefaultController extends Controller
         return $this->render('web/default/index.html.twig', array('state'=> $state , 'medic' => $pagination, 'speciality' => $speciality ));
     }
 
-    public function detailprofile( Request $request){
+    public function showProfileAction( Request $request){
 
         /*
         *   Obtain profile detail by user
@@ -81,7 +81,10 @@ class DefaultController extends Controller
 
         $em     = $this->getDoctrine()->getManager();        
 
-        $RAW_QUERY  = "select md.md_first_name, md.md_first_surname,c.cit_name,s.sta_name,md.md_profile_image,s.sta_code, group_concat(e.sp_name SEPARATOR ', ') as esp, (select count(*) as totla from user_views as userV where userV.vis_usu_id = u.usr_id ) as total from user as u 
+        $RAW_QUERY  = "select  u.usr_id, md.md_first_name,  ci.ci_company,ci.ci_schedule,md.md_first_surname,c.cit_name,s.sta_name,md.md_profile_image,s.sta_code, md.md_profile_description, md.md_academic_training,md.md_professional_experience,md.md_courses_seminars,md.md_aditional_information,
+                        ci.ci_lat,ci.ci_lng,ci.ci_address,ci.ci_phone1, ci.ci_phone2,u.usr_email,
+                        group_concat(e.sp_name SEPARATOR ', ') as esp, (select count(*) as totla 
+                        from user_views as userV where userV.vis_usu_id = u.usr_id ) as total from user as u 
             LEFT JOIN medical_detail as md on u.usr_id = md.usr_id left join contact_info as ci on ci.usr_id = u.usr_id left join city as c on c.cit_id = ci.cit_id
             LEFT JOIN state as s on s.sta_id = c.sta_id
             LEFT JOIN speciality as e on e.usr_id = u.usr_id
@@ -90,9 +93,50 @@ class DefaultController extends Controller
 
         $statement  = $em->getConnection()->prepare($RAW_QUERY);
                       $statement->execute();    
-        $medic      = $statement->fetchAll();        
+        $profile      = $statement->fetchAll();   
 
-        return $this->render('web/default/detail.html.twig', array( 'medic' => $medic ));
+        //Obtain gallery
+
+        $RAW_GALLERY    = "select * from gallery where usr_id = ".$id_profile;
+        $statement      = $em->getConnection()->prepare($RAW_GALLERY);
+                          $statement->execute();
+        $gallery        = $statement->fetchAll();  
+
+        //Obtain Social Network 
+        $RAW_Social     = "select * from user_has_social_network usn left join social_network as sn on 
+                            usn.sn_id = sn.sn_id where usr_id =".$id_profile;
+        $statement      = $em->getConnection()->prepare($RAW_Social);
+                          $statement->execute();
+        $social_network = $statement->fetchAll();  
+
+        $horaDias = array();
+        $hoy = "";
+        if($profile[0]['ci_schedule']!=""){
+            $arr = unserialize($profile[0]['ci_schedule']);
+
+            $num = 1;
+            
+            $horaDias = array();
+            $dias = array(1 => "Mon", 2 => "Tus", 3 => "Wed", 4 => "Thr", 5 => "Fri", 6 => "Sat", 7 => "Sun");
+            $hoy = $dias[date("N")];
+
+            foreach ($arr as $key => $value) 
+            {
+                if ( count($value) > 0 )
+                {
+                    $hd = "";
+                    $cadena = $this->rangos($value);
+                    $hd .= $cadena;
+                    $horaDias[$key] = $hd;
+                }
+                $num++; 
+            }
+            //var_dump($horaDias);
+        }     
+
+        $this->ViewsProfiles( $request , $id_profile );
+
+        return $this->render('web/default/showProfile.html.twig', array( 'profile' => $profile ,"horario" => $horaDias, "hoy" => $hoy, 'gallery' => $gallery , 'social_network' => $social_network ));
     }
 
     public function showFullProfileAction( Request $request ){
@@ -116,14 +160,15 @@ class DefaultController extends Controller
         return $response;
     }
 
-    private function ViewsProfiles(){
+    private function ViewsProfiles( Request $request , $id_profile){
         // Validar si la Cookie se ha seteado
         if(empty($_COOKIE['contador']))
         {
             // Obtener La Ip del visitante.
-            $ip = $this->getRealIP();
+            $em     = $this->getDoctrine()->getManager();        
+            $ip     = $this->getRealIP();
 
-            $oUsr   = $em->getRepository('AppBundle:User')->findOneBy( array('usrId' => 1) );     
+            $oUsr   = $em->getRepository('AppBundle:User')->findOneBy( array('usrId' => $id_profile) );     
 
             $visitas = new UserViews();
             $visitas->setVisUsu( $oUsr );
@@ -133,7 +178,7 @@ class DefaultController extends Controller
             $em->merge($visitas);
             $em->flush();
 
-            //setcookie('contador', 1, time() + 365 * 24 * 60 * 60, $request->getRequestUri());
+            setcookie('contador', 1, time() + 365 * 24 * 60 * 60, $request->getRequestUri());
         }
     }
 
@@ -156,5 +201,77 @@ class DefaultController extends Controller
             }
         }    
         return $sIpAddress;
+    }
+
+    public function calcular_tiempo_trasnc($hora1,$hora2)
+    {
+        $separar[1]=explode(':',$hora1);
+        $separar[2]=explode(':',$hora2);
+
+        $total_minutos_trasncurridos[1] = ($separar[1][0]*60)+$separar[1][1];
+        $total_minutos_trasncurridos[2] = ($separar[2][0]*60)+$separar[2][1];
+        $total_minutos_trasncurridos = $total_minutos_trasncurridos[1]-$total_minutos_trasncurridos[2];
+        if($total_minutos_trasncurridos<=59){
+            //return($total_minutos_trasncurridos.' Minutos');
+            return($total_minutos_trasncurridos);   
+        } 
+        else if($total_minutos_trasncurridos>59)
+        {
+            $HORA_TRANSCURRIDA = round($total_minutos_trasncurridos/60);
+            if($HORA_TRANSCURRIDA<=9)
+            {
+                $HORA_TRANSCURRIDA='0'.$HORA_TRANSCURRIDA;
+            }
+            $MINUITOS_TRANSCURRIDOS = $total_minutos_trasncurridos%60;
+            if($MINUITOS_TRANSCURRIDOS<=9) 
+            {
+                $MINUITOS_TRANSCURRIDOS='0'.$MINUITOS_TRANSCURRIDOS;
+            }   
+            //return ($HORA_TRANSCURRIDA.':'.$MINUITOS_TRANSCURRIDOS.' Horas');
+            return $Minutos = ($HORA_TRANSCURRIDA * 60)+$MINUITOS_TRANSCURRIDOS;
+
+        } 
+    }
+
+    public function rangos($arr)
+    {
+        $pros = 1;
+        $total = count($arr);
+        $cadena = "";
+        $block = false;
+        for($i=0; $i < count($arr); $i++)
+        {
+            if($pros < $total)
+            {
+                $res = abs( $this->calcular_tiempo_trasnc($hora1=$arr[$i],$hora2=$arr[$i+1]) );
+                if( $pros == 1 )
+                {
+                    $cadena .= date('ga', strtotime($arr[$i]));
+                }
+                else
+                {
+                    if( $res != 60 )
+                    {
+                        $cadena .= " <i>a</i> ". date('ga', strtotime($arr[$i]));
+                        $block=true;
+                    }
+                    else
+                    {
+                        if( $block )
+                        {
+                            $cadena .= "<br /> ". date('ga', strtotime($arr[$i]));
+                            $block = false;
+                        }   
+                    }
+                }
+            }
+            else
+            {
+                $cadena .= " <i>a</i> " .date('ga', strtotime($arr[$i]));
+            }
+            $pros++;
+
+        }
+        return $cadena;
     }
 }
