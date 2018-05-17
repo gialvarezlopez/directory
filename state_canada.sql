830e0002098291c1eb6426b40145c5844b35361f
dkrock24 <rgutierreztejada@gmail.com>
2018-05-16 21:43:50 -0600

    Agrando Sates and cities from Canada

---
+++ state_canada.sql
@@ -0,0 +1,16 @@
+
+
+INSERT INTO `states` VALUES ('Alberta', 'AB','53.01669802','-112.8166386');
+INSERT INTO `states` VALUES ('British Columbia', 'BC','49.09996035','-116.516697');
+INSERT INTO `states` VALUES ('Manitoba', 'MB','50.15002545','-96.88332178');
+INSERT INTO `states` VALUES ('New Brunswick', 'NB','45.26704185','-66.07667505');
+INSERT INTO `states` VALUES ('Newfoundland and Labrador', 'NL','49.17440025','-57.42691878');
+INSERT INTO `states` VALUES ('Northwest Territories', 'NT','62.40005292','-110.7333291');
+INSERT INTO `states` VALUES ('Nova Scotia', 'NS','45.58327578','-62.63331934');
+INSERT INTO `states` VALUES ('Nunavut', 'NU','68.76746684','-81.23608303');
+INSERT INTO `states` VALUES ('Ontario', 'ON','44.56664532','-80.84998519');
+INSERT INTO `states` VALUES ('Prince Edward Island', 'PE','46.24928164','-63.13132512');
+INSERT INTO `states` VALUES ('Quebec', 'QC','49.82257774','-64.34799504');
+INSERT INTO `states` VALUES ('Saskatchewan', 'SK','50.93331097','-102.7999891');
+INSERT INTO `states` VALUES ('Yukon Territory', 'YT','61.35037539','-139.0000017');
+
