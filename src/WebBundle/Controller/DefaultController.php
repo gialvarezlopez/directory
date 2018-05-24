3e866b484f83a518f943b0341d12890a7e5c110b
gio <gialvarezlopez@gmail.com>
2018-05-24 14:20:43 +0800

    ajustes en detalle del perfil

--- src/WebBundle/Controller/DefaultController.php
+++ src/WebBundle/Controller/DefaultController.php
@@ -284,8 +284,8 @@ class DefaultController extends Controller
         $separar[1]=explode(':',$hora1);
         $separar[2]=explode(':',$hora2);
 
-        $total_minutos_trasncurridos[1] = ($separar[1][0]*60)+$separar[1][1];
-        $total_minutos_trasncurridos[2] = ($separar[2][0]*60)+$separar[2][1];
+        $total_minutos_trasncurridos[1] = (preg_replace("/[^0-9]/", "", $separar[1][0])*60)+ preg_replace("/[^0-9]/", "", $separar[1][1]);
+        $total_minutos_trasncurridos[2] = (preg_replace("/[^0-9]/", "", $separar[2][0])*60)+preg_replace("/[^0-9]/", "", $separar[2][1]);
         $total_minutos_trasncurridos = $total_minutos_trasncurridos[1]-$total_minutos_trasncurridos[2];
         if($total_minutos_trasncurridos<=59){
             //return($total_minutos_trasncurridos.' Minutos');
