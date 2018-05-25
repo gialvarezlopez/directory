365f8f5ee64f282e4562a970f43bdf355b3f3524
gio <gialvarezlopez@gmail.com>
2018-05-25 13:10:19 +0800

    activando produccion

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,7 +23,7 @@ class AppKernel extends Kernel
             new WebBundle\WebBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['prod','dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
